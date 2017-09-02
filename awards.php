81020d5f97ba14eb33b8b61570e6ca2323ac6576
chad56s <chad56s@gmail.com>
2017-09-01 21:09:01 -0600

    fix awards page to use basic page template

--- awards.php
+++ awards.php
@@ -1,54 +1,65 @@
 <?php
-		include_once ('config.php');
-		include_once('header.php');
+	
+	include_once ('config.php');
+	
+	include_once (OBJECT_ROOT . '/League.php');
+	include_once (APP_ROOT . '/func/leagueUtils.php');
+
+	include_once (UI_ROOT . '/basicLeaguePage.php');
+	
+	
+	ob_start();
+
+	$dataMgr = createNewDataManager();
+	
+	$mode = 'champ';
+	if(isset($_GET['mode']))
+		$mode = $_GET['mode'];
+	
+	echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=champ'>Champion</a>&nbsp;|&nbsp;";
+	echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=weekly'>Weekly and Season Awards</a>&nbsp;|&nbsp;";
+	echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=allpro'>All-Pro Team</a>&nbsp;|&nbsp;";
+	echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=allrookie'>All-Rookie Team</a>";
+	
+	switch ($mode){
 		
-		$dataMgr = createNewDataManager();
-		
-		$mode = 'champ';
-		if(isset($_GET['mode']))
-			$mode = $_GET['mode'];
-		
-		echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=champ'>Champion</a>&nbsp;|&nbsp;";
-		echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=weekly'>Weekly and Season Awards</a>&nbsp;|&nbsp;";
-		echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=allpro'>All-Pro Team</a>&nbsp;|&nbsp;";
-		echo "<a href='" . $_SERVER['PHP_SELF'] . "?mode=allrookie'>All-Rookie Team</a>";
-		
-		switch ($mode){
+		case 'champ':
+
+			$sql = "SELECT * FROM fof_gameresults WHERE Week = " . WEEK_ULTIMATE_BOWL . " AND Year=" . $y;
+			$qResults = $dataMgr->runQuery($sql);
 			
-			case 'champ':
-
-				$sql = "SELECT * FROM fof_gameresults WHERE Week = " . WEEK_ULTIMATE_BOWL . " AND Year=" . $y;
-				$qResults = $dataMgr->runQuery($sql);
+			if(mysql_num_rows($qResults)){
+				$row = mysql_fetch_object($qResults);
 				
-				if(mysql_num_rows($qResults)){
-					$row = mysql_fetch_object($qResults);
+				if($row->AwayScore > $row->HomeScore)
+					$champ = Team::fromId($row->AwayTeam);
+				else
+					$champ = Team::fromId($row->HomeTeam);
 					
-					if($row->AwayScore > $row->HomeScore)
-						$champ = Team::fromId($row->AwayTeam);
-					else
-						$champ = Team::fromId($row->HomeTeam);
-						
-					echo "<h1>UFL " . $y . " Champions</h1>";
-					echo "<img src='" . $champ->getHelmetImg() . "'>";
-					
-				}
-				break;
+				echo "<h1>UFL " . $y . " Champions</h1>";
+				echo "<img src='" . $champ->getHelmetImg() . "'>";
+				
+			}
+			break;
+		
+		case 'allpro':
+			include('stelmack/SeasonAllProTeam.html');
+			break;
+
+		case 'allrookie':
+			include('stelmack/SeasonAllRookieTeam.html');
+			break;
 			
-			case 'allpro':
-				include('stelmack/SeasonAllProTeam.html');
-				break;
+		case 'weekly':
+			include('stelmack/SeasonAwards.html');
+			break;
 
-			case 'allrookie':
-				include('stelmack/SeasonAllRookieTeam.html');
-				break;
-				
-			case 'weekly':
-				include('stelmack/SeasonAwards.html');
-				break;
+		default:
+			echo 'Invalid Mode: ' . $mode;
+	}
 
-			default:
-				echo 'Invalid Mode: ' . $mode;
-		}
+	$awards_page = new BasicLeaguePage();
+	$awards_page->pagePrint(ob_get_clean());
 
 	include('footer.php');
 
